600806a87b7a92500dbee1db251fbfe13f53d1c6
9leaves <152252658+9leaves@users.noreply.github.com>
2025-03-24 15:17:32 +1100

    Add files via upload

---
+++ GPEditSetup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+pushd "%~dp0"
+
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >gp.txt
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>gp.txt
+
+for /f %%i in ('findstr /i . gp.txt 2^>nul') do dism /online /add-package:"%SystemRoot%\servicing\Packages\%%i"
+
+pause
